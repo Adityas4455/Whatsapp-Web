27a9339b679005ac041545ac51f8c639a06eaec5
Adityas4455 <siddhiaditya01@gmail.com>
2025-08-10 19:05:57 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@whatsapp-clone.fqers4o.mongodb.net/`;
+    const URL = process.env.MONGO_URI;
     try {
         await mongoose.connect(URL, { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false });
         console.log('Database Connected Succesfully');
