f07229f47b35ae3743ba0041883551a0928c4a74
Adityas4455 <siddhiaditya01@gmail.com>
2025-08-10 17:31:40 +0530

    Update index.js

--- socket/index.js
+++ socket/index.js
@@ -1,45 +1,51 @@
 import { Server } from 'socket.io';
+import dotenv from 'dotenv';
 
-const io = new Server(9000, {
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:3000';
+
+const io = new Server(PORT, {
     cors: {
-        origin: 'http://localhost:3000',
-    }, 
-})
-
+        origin: CLIENT_URL,
+        methods: ["GET", "POST"]
+    }
+});
 
 let users = [];
 
 const addUser = (userData, socketId) => {
-    !users.some(user => user.sub === userData.sub) && users.push({ ...userData, socketId });
-}
+    !users.some(user => user.sub === userData.sub) &&
+        users.push({ ...userData, socketId });
+};
 
 const removeUser = (socketId) => {
     users = users.filter(user => user.socketId !== socketId);
-}
+};
 
 const getUser = (userId) => {
     return users.find(user => user.sub === userId);
-}
+};
 
-io.on('connection',  (socket) => {
-    console.log('user connected')
+io.on('connection', (socket) => {
+    console.log('user connected');
 
-    //connect
     socket.on("addUser", userData => {
         addUser(userData, socket.id);
         io.emit("getUsers", users);
-    })
+    });
 
-    //send message
     socket.on('sendMessage', (data) => {
         const user = getUser(data.receiverId);
-        io.to(user.socketId).emit('getMessage', data)
-    })
+        if (user) {
+            io.to(user.socketId).emit('getMessage', data);
+        }
+    });
 
-    //disconnect
     socket.on('disconnect', () => {
         console.log('user disconnected');
         removeUser(socket.id);
         io.emit('getUsers', users);
-    })
-})
+    });
+});
